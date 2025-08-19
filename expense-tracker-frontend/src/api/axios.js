b870cf4ebd7956609fcfaf8b59f5085222015d04
Gunasekaraselvapandian <gssp02826@gmail.com>
2025-08-20 01:19:08 +0530

    backend url

--- expense-tracker-frontend/src/api/axios.js
+++ expense-tracker-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://expense-tracker-9kr4.onrender.com/api",
 });
 
 // Add token to requests
