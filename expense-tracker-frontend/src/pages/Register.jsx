f1c2665fb1359dbbb0b2d10baee62b8c6576da1b
Gunasekaraselvapandian <gssp02826@gmail.com>
2025-08-20 15:47:26 +0530

    #

--- expense-tracker-frontend/src/pages/Register.jsx
+++ expense-tracker-frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router";
-import axios from "../api/axios";
+import api from "../api/axios";
 
 export default function Register() {
   const [username, setUsername] = useState("");
